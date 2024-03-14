9ac87fa212c085cfbf207af0df387c5350cccabf
LakshmiN17 <161114969+LakshmiN17@users.noreply.github.com>
2024-03-14 18:17:33 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,18 @@
+class demo
+{
+	public static void main(String[] args)
+	{
+		int num=0;	
+		if (num>0){
+			System.out.println(+num+ "is positive number");
+		}
+		else if(num<0){
+			System.out.println(+num+ "is negative number");
+		}
+		else{
+			System.out.println(+num+ "is not positive or negative");
+		}
+	}
+}
+		
+		
